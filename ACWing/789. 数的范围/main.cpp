a84ac9b093bf1d1afe4e4d9dc54f5b282d080dc7
edwardwang618 <edward.wang618@gmail.com>
2021-02-11 10:58:12 -0600

    ac 789

---
+++ ACWing/789. 数的范围/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+const int N = 100010;
+int n, q, k;
+int a[N];
+
+int main() {
+    scanf("%d%d", &n, &q);
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
+    int x, y;
+    while (q--) {
+        cin >> k;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int m = l + (r - l >> 1);
+            if (a[m] >= k) r = m;
+            else l = m + 1;
+        }
+
+        if (a[l] != k) {
+            printf("-1 -1\n");
+            continue;
+        }
+
+        x = l;
+        l = 0, r = n - 1;
+        while (l < r) {
+            int m = l + (r - l + 1 >> 1);
+            if (a[m] <= k) l = m;
+            else r = m - 1;
+        }
+        y = l;
+
+        printf("%d %d\n", x, y);
+    }
+
+    return 0;
+}
