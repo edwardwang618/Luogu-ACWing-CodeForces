bfc8131d2b55f377f59156abd0776155eec1cf94
edwardwang618 <edward.wang618@gmail.com>
2021-02-13 01:54:36 -0600

    ac 154

---
+++ ACWing/154. 滑动窗口/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+const int N = 1000010;
+int a[N];
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    deque<int> dq;
+
+    for (int i = 0; i < n; i++) {
+        while (!dq.empty() && a[dq.back()] >= a[i]) dq.pop_back();
+        dq.push_back(i);
+        if (dq.front() <= i - k) dq.pop_front();
+        if (i >= k - 1) cout << a[dq.front()] << " ";
+    }
+
+    cout << endl;
+    dq.clear();
+    for (int i = 0; i < n; i++) {
+        while (!dq.empty() && a[dq.back()] <= a[i]) dq.pop_back();
+        dq.push_back(i);
+        if (dq.front() <= i - k) dq.pop_front();
+        if (i >= k - 1) cout << a[dq.front()] << " ";
+    }
+
+    return 0;
+}
