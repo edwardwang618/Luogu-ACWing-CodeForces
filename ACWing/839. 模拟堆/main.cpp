ee03453b845603fe14cd2307f4e6437027952ef9
edwardwang618 <edward.wang618@gmail.com>
2021-02-13 22:01:09 -0600

    ac 839

---
+++ ACWing/839. 模拟堆/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+const int N = 100010;
+// ph从下表映射到堆，hp从堆映射到下标
+int h[N], ph[N], hp[N], s;
+
+void heap_swap(int a, int b) {
+    swap(ph[hp[a]], ph[hp[b]]);
+    swap(hp[a], hp[b]);
+    swap(h[a], h[b]);
+}
+
+void down(int u) {
+    while (u * 2 <= s) {
+        int t = u;
+        if (h[u * 2] < h[t]) t = u * 2;
+        if (u * 2 + 1 <= s && h[u * 2 + 1] < h[t]) t = u * 2 + 1;
+        if (u != t) {
+            heap_swap(u, t);
+            u = t;
+        } else break;
+    }
+}
+
+void up(int u) {
+    while (u >> 1 && h[u >> 1] > h[u]) {
+        heap_swap(u >> 1, u);
+        u >>= 1;
+    }
+}
+
+int main() {
+    int n, m = 0;
+    cin >> n;
+
+    while (n--) {
+        char op[10];
+        int k, x;
+        scanf("%s", op);
+        if (!strcmp(op, "I")) {
+            cin >> x;
+            s++;
+
+            m++;
+            ph[m] = s;
+            hp[s] = m;
+            h[s] = x;
+            up(s);
+        } else if (!strcmp(op, "PM")) cout << h[1] << endl;
+        else if (!strcmp(op, "DM")) {
+            heap_swap(1, s);
+            s--;
+            down(1);
+        } else if (!strcmp(op, "D")) {
+            cin >> k;
+            k = ph[k];
+            heap_swap(k, s);
+            s--;
+            down(k), up(k);
+        } else if (!strcmp(op, "C")) {
+            cin >> k >> x;
+            k = ph[k];
+            h[k] = x;
+            down(k), up(k);
+        }
+    }
+
+    return 0;
+}
---
+++ ACWing/844. 走迷宫/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+typedef pair<int, int> PII;
+const int N = 110;
+int a[N][N];
+bool visited[N][N];
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= n; j++)
+            cin >> a[i][j];
+
+    if (m == 1 && n == 1) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    int res = 0;
+    queue<PII> q;
+    q.push({1, 1});
+    int d[] = {1, 0, -1, 0, 1};
+    while (!q.empty()) {
+        res++;
+        int s = q.size();
+        for (int i = 0; i < s; i++) {
+            PII pair = q.front();
+            q.pop();
+            for (int j = 0; j < 4; j++) {
+                int nextX = pair.first + d[j], nextY = pair.second + d[j + 1];
+                if (nextX == m && nextY == n) {
+                    cout << res << endl;
+                    return 0;
+                }
+
+                if (1 <= nextX && nextX <= m && 1 <= nextY && nextY <= n && a[nextX][nextY] == 0 && !visited[nextX][nextY]) {
+                    visited[nextX][nextY] = true;
+                    q.push({nextX, nextY});
+                }
+            }
+        }
+    }
+
+    return 0;
+}
