75550657ef7abef93f1a4a1cf71e93e69ef86548
edwardwang618 <edward.wang618@gmail.com>
2021-02-15 01:01:31 -0600

    ac 849

---
+++ ACWing/849. Dijkstra求最短路 I/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int N = 510;
+
+int n, m;
+int g[N][N];
+int dist[N];
+// st[i]存第i个点是否确定了最短路
+bool st[N];
+
+int dijkstra() {
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+
+    for (int i = 0; i < n; i++) {
+        int t = -1;
+        for (int j = 1; j <= n; j++) 
+            if (!st[j] && (t == -1 || dist[t] > dist[j])) t = j;
+        
+        st[t] = true;
+        for (int j = 1; j <= n; j++) 
+            if (!st[j])
+                dist[j] = min(dist[j], dist[t] + g[t][j]);
+    }
+
+    return dist[n] == 0x3f3f3f3f ? -1 : dist[n];
+}
+
+int main() {
+    cin >> n >> m;
+
+    memset(g, 0x3f, sizeof g);
+
+    while (m--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a == b) g[a][b] = 0;
+        else g[a][b] = min(g[a][b], c);
+    }
+
+    cout << dijkstra() << endl;
+
+    return 0;
+}
