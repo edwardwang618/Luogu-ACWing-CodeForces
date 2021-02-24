870470bee32635230523481e842659e613ba4fe7
edwardwang618 <edward.wang618@gmail.com>
2021-02-23 22:26:09 -0600

    ac 894

--- ACWing/894. 拆分-Nim游戏/main.cpp
+++ ACWing/894. 拆分-Nim游戏/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <cstring>
+#include <unordered_set>
 using namespace std;
 
-int main() {
-    
+const int N = 110;
 
+int f[N];
+
+int sg(int x) {
+    if (f[x] != -1) return f[x];
+
+    unordered_set<int> set;
+    for (int i = 0; i < x; i++) 
+        for (int j = 0; j <= i; j++)
+            set.insert(sg(i) ^ sg(j));
+
+    for (int i = 0; ; i++) 
+        if (!set.count(i)) return f[x] = i;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    memset(f, -1, sizeof f);
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        res ^= sg(x);
+    }
+
+    cout << (res != 0 ? "Yes" : "No") << endl;
 
     return 0;
 }
