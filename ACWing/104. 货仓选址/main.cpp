38bd5de48d3193eeae6d691639963cb93e905dcf
edwardwang618 <edward.wang618@gmail.com>
2021-02-26 01:47:14 -0600

    ac 104

--- ACWing/104. 货仓选址/main.cpp
+++ ACWing/104. 货仓选址/main.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 const int N = 100010;
-int n, k;
 int a[N];
 
 int quick_select(int l, int r, int idx) {
     int i = l, j = r;
     int m = a[l + (r - l >> 1)];
-    do {
+    do
+    {
         while (a[i] < m) i++;
         while (a[j] > m) j--;
         if (i <= j) {
@@ -17,16 +17,22 @@ int quick_select(int l, int r, int idx) {
             j--;
         }
     } while (i <= j);
-
+    
     if (idx <= j) return quick_select(l, j, idx);
     else if (idx >= i) return quick_select(i, r, idx);
     else return a[idx];
 }
 
 int main() {
-    cin >> n >> k;
-    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
 
-    cout << quick_select(0, n - 1, k - 1) << endl;
+    int res = 0;
+    int x = quick_select(0, n - 1, n / 2);
+    for (int i = 0; i < n; i++) res += abs(a[i] - x);
+
+    cout << res << endl;
+
     return 0;
 }
