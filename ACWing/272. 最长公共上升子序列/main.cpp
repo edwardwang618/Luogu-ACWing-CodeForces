788d8f26baa6d9c2fc0652b98db9cae765c8d0f0
edwardwang618 <edward.wang618@gmail.com>
2021-02-28 12:17:45 -0600

    ac 272

---
+++ ACWing/272. 最长公共上升子序列/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+const int N = 3010;
+int n;
+int a[N], b[N];
+int f[N][N];
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+    for (int i = 1; i <= n; i++) scanf("%d", &b[i]);
+
+    for (int i = 1; i <= n; i++) {
+        int maxv = 0;
+        for (int j = 1; j <= n; j++) {
+            f[i][j] = f[i - 1][j];
+            if (a[i] == b[j]) f[i][j] = max(f[i][j], maxv + 1);
+            if (b[j] < a[i]) maxv = max(maxv, f[i][j]);
+        }
+    }
+
+    int res = 0;
+    for (int i = 1; i <= n; i++) res = max(res, f[n][i]);
+
+    cout << res << endl;
+
+    return 0;
+}
