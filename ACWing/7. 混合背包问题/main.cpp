8ab9e8a677cf9b2b2d64c180012375201fa0f0af
edwardwang618 <edward.wang618@gmail.com>
2021-03-02 15:27:49 -0600

    ac 7

---
+++ ACWing/7. 混合背包问题/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+const int N = 1010;
+int n, m;
+int f[N];
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        int v, w, s;
+        cin >> v >> w >> s;
+        if (s == 0) 
+            for (int j = v; j <= m; j++)
+                f[j] = max(f[j], f[j - v] + w);
+        else {
+            if (s == -1) s = 1;
+            // 二进制优化
+            for (int k = 1; k <= s; k *= 2) {
+                for (int j = m; j >= k * v; j--) 
+                    f[j] = max(f[j], f[j - k * v] + k * w);
+                s -= k;
+            }
+
+            if (s)
+                for (int j = m; j >= s * v; j--)
+                    f[j] = max(f[j], f[j - s * v] + s * w);
+        }
+    }    
+
+    cout << f[m] << endl;
+
+    return 0;
+}
