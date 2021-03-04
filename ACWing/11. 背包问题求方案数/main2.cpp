6a859827f993d86fe15fe35bd809131991e6e13c
edwardwang618 <edward.wang618@gmail.com>
2021-03-04 14:05:43 -0600

    ac 114

--- ACWing/11. 背包问题求方案数/main2.cpp
+++ ACWing/11. 背包问题求方案数/main2.cpp
@@ -8,9 +8,7 @@ int f[N], g[N];
 
 int main() {
     cin >> n >> m;
-    memset(f, -0x3f, sizeof f);
-    f[0] = 0;
-    g[0] = 1;
+    for (int i = 0; i <= m; i++) g[i] = 1;
 
     for (int i = 1; i <= n; i++) {
         int v, w;
@@ -25,15 +23,7 @@ int main() {
         }
     }
 
-    int res = 0;
-    for (int i = 0; i <= m; i++) res = max(res, f[i]);
-
-    int cnt = 0;
-    for (int i = 0; i <= m; i++)
-        if (f[i] == res)
-            cnt = (cnt + g[i]) % mod;
-
-    cout << cnt << endl;
+    cout << g[m] << endl;
 
     return 0;
 }
---
+++ ACWing/114. 国王游戏/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int N = 1010;
+pair<int, int> p[N];
+int n;
+
+vector<int> mul(vector<int> a, int b) {
+    vector<int> c;
+    for (int i = 0, t = 0; i < a.size() || t; i++) {
+        if (i < a.size()) t += a[i] * b;
+        c.push_back(t % 10);
+        t /= 10;
+    }
+
+    return c;
+}
+
+vector<int> div(vector<int> a, int b) {
+    vector<int> c;
+    for (int i = a.size() - 1, t = 0; i >= 0; i--) {
+        t = t * 10 + a[i];
+        int d = t / b;
+        if (d || !c.empty()) c.push_back(d);
+        t %= b;
+    }
+
+    reverse(c.begin(), c.end());
+    if (c.empty()) c.push_back(0);
+    return c;
+}
+
+vector<int> maxv(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) return a.size() > b.size() ? a : b;
+    for (int i = a.size() - 1; i >= 0; i--) 
+        if (a[i] != b[i]) return a[i] > b[i] ? a : b;
+
+    return a;
+}
+
+int main() {
+    cin >> n;
+    cin >> p[0].first >> p[0].second;
+
+    for (int i = 1; i <= n; i++) {
+        int x, y;
+        cin >> x >> y;
+        p[i] = {x * y, y};
+    }
+
+    sort(p + 1, p + n + 1);
+
+    vector<int> res(1, 0);
+    vector<int> prod(1, 1);
+    prod = mul(prod, p[0].first);
+
+    for (int i = 1; i <= n; i++) {
+        res = maxv(res, div(prod, p[i].second));
+        prod = mul(prod, p[i].first / p[i].second);
+    }
+
+    for (int i = res.size() - 1; i >= 0; i--)
+        cout << res[i];
+    cout << endl;
+
+    return 0;
+}
