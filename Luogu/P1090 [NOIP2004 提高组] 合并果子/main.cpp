44a2aed6a11a295a1ca4825fe06d7faeee3c250c
edwardwang618 <edward.wang618@gmail.com>
2021-04-21 12:31:55 -0400

    lg 1090

---
+++ Luogu/P1090 [NOIP2004 提高组] 合并果子/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    priority_queue<int, vector<int>, greater<int> > heap;
+    while (n--) {
+        int a;
+        cin >> a;
+        heap.push(a);
+    }
+
+    int res = 0;
+    while (heap.size() >= 2) {
+        int a = heap.top(); heap.pop();
+        int b = heap.top(); heap.pop();
+
+        res += a + b;
+        heap.push(a + b);
+    }
+    
+    cout << res << endl;
+}
