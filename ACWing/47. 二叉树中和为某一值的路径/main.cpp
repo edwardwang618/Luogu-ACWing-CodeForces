72cecd7e37840c8b4ebd98e7fe8d39c57bd2b64e
edwardwang618 <edward.wang618@gmail.com>
2021-04-24 17:39:33 -0400

    ac 47

---
+++ ACWing/47. 二叉树中和为某一值的路径/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    vector<vector<int> > res;
+    vector<int> path;
+
+    vector<vector<int> > findPath(TreeNode* root, int sum) {
+        dfs(root, 0, sum);
+        return res;
+    }
+
+    void dfs(TreeNode *cur, int s, int sum) {
+        if (!cur) return;
+
+        s += cur->val;
+        path.push_back(cur->val);
+        if (!cur->left && !cur->right) {
+            if (s == sum) res.push_back(path);
+            path.pop_back();
+            return;
+        }
+
+        if (cur->left) dfs(cur->left, s, sum);
+        if (cur->right) dfs(cur->right, s, sum);
+        path.pop_back();
+    }
+};
