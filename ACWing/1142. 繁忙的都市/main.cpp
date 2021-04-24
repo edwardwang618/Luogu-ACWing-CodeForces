66034b4e6c9d6be371fd6d0c2f4ce8277e6f70c9
edwardwang618 <edward.wang618@gmail.com>
2021-04-24 14:13:22 -0400

    ac 1142

--- ACWing/1142. 繁忙的都市/main.cpp
+++ ACWing/1142. 繁忙的都市/main.cpp
@@ -2,43 +2,45 @@
 #include <algorithm>
 using namespace std;
 
-const int N = 110, M = 210;
+const int N = 310, M = 8010;
 int n, m;
+int cnt, ma;
 
 struct Edge {
     int a, b, w;
-    bool operator<(const Edge &t) const {
+    bool operator<(Edge &t) const {
         return w < t.w;
     }
 } e[M];
-
 int p[N];
 
-int find(int a) {
-    if (a != p[a]) p[a] = find(p[a]);
-    return p[a];
+int find(int x) {
+    if (x != p[x]) p[x] = find(p[x]);
+    return p[x];
 }
 
 int main() {
     cin >> n >> m;
-    for (int i = 1; i <= n; i++) p[i] = i;
-
     for (int i = 0; i < m; i++) {
         int a, b, w;
         cin >> a >> b >> w;
         e[i] = {a, b, w};
     }
 
+    for (int i = 1; i <= n; i++) p[i] = i;
+
     sort(e, e + m);
 
-    int res = 0;
     for (int i = 0; i < m; i++) {
-        int a = find(e[i].a), b = find(e[i].b), w = e[i].w;
-        if (a != b) p[a] = b;
-        else res += w;
+        int pa = find(e[i].a), pb = find(e[i].b);
+        if (pa != pb) {
+            p[pa] = pb;
+            cnt++;
+            ma = e[i].w;
+        }
     }
 
-    cout << res << endl;
+    printf("%d %d\n", cnt, ma);
 
     return 0;
 }
