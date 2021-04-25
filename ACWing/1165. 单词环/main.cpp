a4afd9f3cac3d46ffccb73afe8b6071b426fb5f2
edwardwang618 <edward.wang618@gmail.com>
2021-04-25 15:06:16 -0400

    lg 1051

--- ACWing/1165. 单词环/main.cpp
+++ ACWing/1165. 单词环/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
-int main() {
-    
+const int N = 700, M = 100010;
+int n;
+int h[N], e[M], w[M], ne[M], idx;
+int cnt[N];
+bool st[N];
 
+void add(int a, int b, int c) {
+
+}
+
+int main() {
+    char str[1010];
+    while (scanf("%d", &n), n) {
+        memset(h, -1, sizeof h);
+        idx = 0;
+        for (int i = 0; i < n; i++) {
+            scanf("%s", str);
+            int len = strlen(str);
+            if (len >= 2) {
+                int left = (str[0] - 'a') * 26 + str[1] - 'a';
+                int right = (str[len - 2] - 'a') * 26 + str[len - 1] - 'a';
+                add(left, right, len);
+            }
+        }
+    }
 
 
     return 0;
---
+++ Luogu/P1051 谁拿了最多奖学金/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student {
+    string name;
+    int qimo;
+    int pingyi;
+    bool ganbu;
+    bool xibu;
+    int lunwen;
+    int jiangjin = 0;
+};
+
+int main() {
+    int N;
+    cin >> N;
+
+    Student no1;
+    no1.jiangjin = 0;
+    int sum = 0;
+    for (int i = 0; i < N; ++i) {
+        Student s;
+        cin >> s.name >> s.qimo >> s.pingyi;
+        string bo;
+        cin >> bo;
+        if (bo == "Y") s.ganbu = true;
+        else s.ganbu = false;
+        cin >> bo;
+        if (bo == "Y") s.xibu = true;
+        else s.xibu = false;
+
+        cin >> s.lunwen;
+        if (s.qimo > 80 && s.lunwen >= 1) s.jiangjin += 8000;
+        if (s.qimo > 85 && s.pingyi > 80) s.jiangjin += 4000;
+        if (s.qimo > 90) s.jiangjin += 2000;
+        if (s.qimo > 85 && s.xibu) s.jiangjin += 1000;
+        if (s.pingyi > 80 && s.ganbu) s.jiangjin += 850;
+        sum += s.jiangjin;
+        if (s.jiangjin > no1.jiangjin) no1 = s;
+    }
+
+    cout << no1.name << endl << no1.jiangjin << endl << sum << endl;
+
+    return 0;
+}
