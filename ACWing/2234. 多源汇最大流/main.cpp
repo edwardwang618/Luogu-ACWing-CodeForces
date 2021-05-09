d1c39499ca8b8005122ed0b3f7379847f33192bc
edwardwang618 <edward.wang618@gmail.com>
2021-05-09 19:54:37 -0400

    ac 2234

--- ACWing/2234. 多源汇最大流/main.cpp
+++ ACWing/2234. 多源汇最大流/main.cpp
@@ -2,9 +2,83 @@
 #include <cstring>
 using namespace std;
 
+const int N = 1e4 + 10, M = (1e5 + N) * 2, INF = 1e8;
+int n, m, S, T;
+int h[N], e[M], f[M], ne[M], idx;
+int q[N], d[N], cur[N];
+
+void add(int a, int b, int c) {
+    e[idx] = b, ne[idx] = h[a], f[idx] = c, h[a] = idx++;
+    e[idx] = a, ne[idx] = h[b], f[idx] = 0, h[b] = idx++;
+}
+
+bool bfs() {
+    memset(d, -1, sizeof d);
+
+    int hh = 0, tt = 0;
+    q[tt++] = S, d[S] = 0, cur[S] = h[S];
+    while (hh < tt) {
+        int t = q[hh++];
+        for (int i = h[t]; ~i; i = ne[i]) {
+            int v = e[i];
+            if (d[v] == -1 && f[i]) {
+                d[v] = d[t] + 1;
+                if (v == T) return true;
+
+                cur[v] = h[v];
+                q[tt++] = v;
+            }
+        }
+    }
+    
+    return false;
+}
+
+int dfs(int u, int limit) {
+    if (u == T) return limit;
+    int flow = 0;
+    for (int i = cur[u]; ~i && flow < limit; i = ne[i]) {
+        cur[u] = i;
+        int v = e[i];
+        if (d[v] == d[u] + 1 && f[i]) {
+            int t = dfs(v, min(limit - flow, f[i]));
+            if (!t) d[v] = -1;
+            f[i] -= t, f[i ^ 1] += t, flow += t;
+        }
+    }
+
+    return flow;
+}
+
+int dinic() {
+    int r = 0, flow;
+    while (bfs()) while (flow = dfs(S, INF)) r += flow;
+    return r;
+}
+
 int main() {
+    int sc, tc;
+    scanf("%d%d%d%d", &n, &m, &sc, &tc);
+    S = 0, T = n + 1;
 
-
-
+    memset(h, -1, sizeof h);
+    while (sc--) {
+        int x;
+        scanf("%d", &x);
+        add(S, x, INF);
+    }
+    while (tc--) {
+        int x;
+        scanf("%d", &x);
+        add(x, T, INF);
+    }
+    
+    while (m--) {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        add(a, b, c);
+    }
+    
+    printf("%d\n", dinic());
     return 0;
 }
