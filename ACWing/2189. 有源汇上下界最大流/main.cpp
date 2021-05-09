ad564fd664d0eb77842dc1a597ab2513d109f053
edwardwang618 <edward.wang618@gmail.com>
2021-05-09 01:30:47 -0400

    ac 2189

--- ACWing/2189. 有源汇上下界最大流/main.cpp
+++ ACWing/2189. 有源汇上下界最大流/main.cpp
@@ -1,10 +1,86 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int main() {
+const int N = 210, M = (N + 10000) * 2, INF = 1e8;
+int n, m, S, T;
+int h[N], e[M], f[M], ne[M], idx;
+int q[N], d[N], cur[N], diff[N];
+
+void add(int a, int b, int c) {
+    e[idx] = b, ne[idx] = h[a], f[idx] = c, h[a] = idx++;
+    e[idx] = a, ne[idx] = h[b], f[idx] = 0, h[b] = idx++;
+}
+
+bool bfs() {
+    int hh = 0, tt = 0;
+    memset(d, -1, sizeof d);
+    q[tt++] = S, d[S] = 0, cur[S] = h[S];
+    while (hh < tt) {
+        int t = q[hh++];
+        for (int i = h[t]; ~i; i = ne[i]) {
+            int v = e[i];
+            if (d[v] == -1 && f[i]) {
+                d[v] = d[t] + 1;
+                if (v == T) return true;
+
+                cur[v] = h[v];
+                q[tt++] = v;
+            }
+        }
+    }
     
+    return false;
+}
 
+int dfs(int u, int limit) {
+    if (u == T) return limit;
+    int flow = 0;
+    for (int i = cur[u]; ~i && flow < limit; i = ne[i]) {
+        cur[u] = i;
+        int v = e[i];
+        if (d[v] == d[u] + 1 && f[i]) {
+            int t = dfs(v, min(limit - flow, f[i]));
+            if (!t) d[v] = -1;
+            f[i] -= t, f[i ^ 1] += t, flow += t;
+        }
+    }
 
+    return flow;
+}
+
+int dinic() {
+    int r = 0, flow;
+    while (bfs()) while (flow = dfs(S, INF)) r += flow;
+    return r;
+}
+
+int main() {
+    int s, t;
+    scanf("%d%d%d%d", &n, &m, &s, &t);
+    S = 0, T = n + 1;
+
+    memset(h, -1, sizeof h);
+    while (m--) {
+        int a, b, c, d;
+        scanf("%d%d%d%d", &a, &b, &c, &d);
+        add(a, b, d - c);
+        diff[a] -= c, diff[b] += c;
+    }
+    
+    int tot = 0;
+    for (int i = 1; i <= n; i++) 
+        if (diff[i] > 0) add(S, i, diff[i]), tot += diff[i];
+        else if (diff[i] < 0) add(i, T, -diff[i]);
+    
+    add(t, s, INF);
+    if (dinic() < tot) printf("No Solution\n");
+    else {
+        int res = f[idx - 1];
+        S = s, T = t;
+        f[idx - 1] = f[idx - 2] = 0;
+        printf("%d\n", res + dinic());
+    }
 
     return 0;
 }
