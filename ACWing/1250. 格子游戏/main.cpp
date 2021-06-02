901c2a8c29e4daf1c0f6c0bbceb08e8886556456
edwardwang618 <edward.wang618@gmail.com>
2021-06-02 16:39:45 -0400

    ac 1250

--- ACWing/1250. 格子游戏/main.cpp
+++ ACWing/1250. 格子游戏/main.cpp
@@ -1,1 +1,44 @@
 #include <iostream>
+#include <cstring>
+using namespace std;
+
+const int N = 40010;
+int n, m;
+int p[N];
+
+int find(int x) {
+    if (p[x] != x) p[x] = find(p[x]);
+    return p[x];
+}
+
+int get(int x, int y) {
+    return x * n + y;
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n * n; i++) p[i] = i;
+
+    int res = 0;
+    for (int i = 1; i <= m; i++) {
+        int x, y;
+        char d;
+        cin >> x >> y >> d;
+        x--, y--;
+        int a = get(x, y), b;
+        if (d == 'D') b = get(x + 1, y);
+        else b = get(x, y + 1);
+
+        int pa = find(a), pb = find(b);
+        if (pa == pb) {
+            res = i;
+            break;
+        }
+        p[pa] = pb;
+    }
+
+    if (!res) puts("draw");
+    else printf("%d\n", res);
+
+    return 0;
+}
