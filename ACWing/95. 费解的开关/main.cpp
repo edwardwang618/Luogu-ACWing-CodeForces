4369f4c1ccbd4b9ca84a70421529f70dc02d5872
edwardwang618 <edward.wang618@gmail.com>
2021-07-24 13:31:17 -0400

    ac 97

--- ACWing/95. 费解的开关/main.cpp
+++ ACWing/95. 费解的开关/main.cpp
@@ -10,7 +10,7 @@ void flip(char& x) {
 }
 
 void click(int x, int y) {
-    g[x][y] = 1 - g[x][y] + 2 * '0';
+    flip(g[x][y]);
     if (x - 1 >= 0) flip(g[x - 1][y]);
     if (x + 1 < 5) flip(g[x + 1][y]);
     if (y - 1 >= 0) flip(g[x][y - 1]);
