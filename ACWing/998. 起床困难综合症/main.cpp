e5dfeda1e5837bc039e688640cf39ecccbb41eac
edwardwang618 <edward.wang618@gmail.com>
2021-12-10 12:37:18 -0800

    ac 998

---
+++ ACWing/998. 起床困难综合症/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int n, m;
+
+int main() {
+    bitset<40> zero, one;
+    zero.reset();
+    one.set();
+    scanf("%d%d", &n, &m);
+    while (n--) {
+        string s;
+        int x;
+        cin >> s >> x;
+        if (s == "AND") zero &= x, one &= x;
+        else if (s == "OR") zero |= x, one |= x;
+        else zero ^= x, one ^= x;
+    }
+    
+    int res = 0;
+    for (int i = 30; i >= 0; i--)
+        if (zero[i] || (one[i] && m >= 1 << i)) res |= 1 << i;
+    
+    printf("%d\n", res);
+
+    return 0;
+}
