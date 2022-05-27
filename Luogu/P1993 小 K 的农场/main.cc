45a7486ab38b8c2decb80a61425f975cd8c6db88
edwardwang618 <edward.wang618@gmail.com>
2022-05-27 13:50:42 -0700

    lg 1993

--- Luogu/P1993 小 K 的农场/main.cc
+++ Luogu/P1993 小 K 的农场/main.cc
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <cstring>
-#include <queue>
+#include <stack>
 using namespace std;
 
-const int N = 5010, M = N;
+const int N = 5e3 + 10, M = N << 1;
 int n, m;
 int h[N], e[M], ne[M], w[M], idx;
 int dist[N], cnt[N];
@@ -14,14 +14,14 @@ void add(int a, int b, int c) {
 }
 
 bool spfa() {
-  queue<int> q;
+  stack<int> stk;
   for (int i = 1; i <= n; i++) {
-    q.push(i);
+    stk.push(i);
     vis[i] = true;
   }
 
-  while (q.size()) {
-    int t = q.front(); q.pop();
+  while (stk.size()) {
+    int t = stk.top(); stk.pop();
     vis[t] = false;
     for (int i = h[t]; ~i; i = ne[i]) {
       int v = e[i];
@@ -30,7 +30,7 @@ bool spfa() {
         cnt[v] = cnt[t] + 1;
         if (cnt[v] > n - 1) return false;
         if (!vis[v]) {
-          q.push(v);
+          stk.push(v);
           vis[v] = true;
         }
       }
@@ -45,12 +45,18 @@ int main() {
 
   scanf("%d%d", &n, &m);
   for (int i = 1; i <= m; i++) {
-    int a, b, c;
-    scanf("%d%d%d", &a, &b, &c);
-    add(b, a, c);
+    int type, a, b, c;
+    scanf("%d", &type);
+    if (type == 3) {
+      scanf("%d%d", &a, &b);
+      add(a, b, 0), add(b, a, 0);
+    } else {
+      scanf("%d%d%d", &a, &b, &c);
+      if (type == 1) add(a, b, -c);
+      else add(b, a, c);
+    }
   }
 
-  bool found = spfa();
-  if (!found) puts("NO");
-  else for (int i = 1; i <= n; i++) printf("%d ", dist[i]);
+  if (spfa()) puts("Yes");
+  else puts("No");
 }
