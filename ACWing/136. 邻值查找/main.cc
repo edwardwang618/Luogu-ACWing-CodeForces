6709b4d6047e0000b26d5cc565b919af4d7e8d2c
edwardwang618 <edward.wang618@gmail.com>
2022-06-11 09:49:26 -0700

    ac 136

---
+++ ACWing/136. 邻值查找/main.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#define x first
+#define y second
+using namespace std;
+using PII = pair<int, int>;
+
+const int N = 1e5 + 10, INF = 1e9 + 1;
+int n;
+int p[N], l[N], r[N];
+PII a[N], res[N];
+
+int main() {
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    scanf("%d", &a[i].x);
+    a[i].y = i;
+  }
+
+  sort(a + 1, a + 1 + n);
+  a[0].first = INF, a[n + 1].first = -INF;
+  r[0] = 1, l[n + 1] = n;
+  for (int i = 1; i <= n; i++) {
+    l[i] = i - 1, r[i] = i + 1;
+    p[a[i].y] = i;
+  }
+
+  for (int i = n; i > 1; i--) {
+    int pos = p[i], left = l[pos], right = r[pos];
+    int lv = abs(a[pos].x - a[left].x), rv = abs(a[pos].x - a[right].x);
+    if (lv < rv) res[i] = {lv, a[left].y};
+    else if (lv > rv) res[i] = {rv, a[right].y};
+    else res[i] = {lv, a[left].x < a[right].x ? a[left].y : a[right].y};
+    r[left] = right, l[right] = left;
+  }
+
+  for (int i = 2; i <= n; i++) printf("%d %d\n", res[i].first, res[i].second);
+}
--- Luogu/P2072 宗教问题/main.cc
+++ Luogu/P2072 宗教问题/main.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstring>
-
 using namespace std;
 
 const int N = 1010;
