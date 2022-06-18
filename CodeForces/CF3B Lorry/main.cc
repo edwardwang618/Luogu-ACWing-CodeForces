62b192705f35fe10e28cac772ca67c55b6c3c116
edwardwang618 <edward.wang618@gmail.com>
2022-06-18 12:56:09 -0700

    cf 3b

---
+++ CodeForces/CF3B Lorry/main.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+using PII = pair<int, int>;
+
+const int N = 1e5 + 10;
+int n, v;
+PII v1[N], v2[N];
+int cnt1, cnt2;
+int pre[N];
+int res, max1, max2;
+
+int main() {
+  scanf("%d%d", &n, &v);
+  for (int i = 1; i <= n; i++) {
+    int t, p;
+    scanf("%d%d", &t, &p);
+    if (t == 1) v1[++cnt1] = {p, i};
+    else v2[++cnt2] = {p, i};
+  }
+
+  sort(v1 + 1, v1 + 1 + cnt1, greater<>());
+  sort(v2 + 1, v2 + 1 + cnt2, greater<>());
+
+  for (int i = 1; i <= cnt2; i++) {
+    pre[i] = v2[i].first + pre[i - 1];
+    if (i << 1 <= v) {
+      res = pre[i];
+      max2 = i;
+    }
+  }
+
+  for (int i = 1, sum = 0; i <= min(cnt1, v); i++) {
+    sum += v1[i].first;
+    int j = min(v - i >> 1, cnt2);
+    if (sum + pre[j] > res) {
+      res = sum + pre[j];
+      max1 = i;
+      max2 = j;
+    }
+  }
+
+  printf("%d\n", res);
+  for (int i = 1; i <= max1; i++) printf("%d ", v1[i].second);
+  for (int i = 1; i <= max2; i++) printf("%d ", v2[i].second);
+}
