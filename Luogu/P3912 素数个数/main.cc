1f34c5b08a5e0b887ec1028c3bb1af0d92ce5fad
edwardwang618 <edward.wang618@gmail.com>
2022-07-04 17:43:31 -0700

    lg 3912

---
+++ Luogu/P3912 素数个数/main.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+const int N = 1e8 + 10;
+int n;
+int p[N], cnt;
+bool st[N];
+
+void get_prime() {
+  for (int i = 2; i <= n; i++) {
+    if (!st[i]) p[cnt++] = i;
+    for (int j = 0; p[j] <= n / i; j++) {
+      st[p[j] * i] = true;
+      if (i % p[j] == 0) break;
+    }
+  }
+}
+
+int main() {
+  scanf("%d", &n);
+  get_prime();
+  printf("%d\n", cnt);
+}
