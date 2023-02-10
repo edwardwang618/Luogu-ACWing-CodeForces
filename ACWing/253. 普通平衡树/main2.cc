5d756a100582500d113d8d52713b75d207f1f45d
edwardwang618 <edward.wang618@gmail.com>
2023-02-09 22:31:18 -0800

    lg 2343

--- ACWing/253. 普通平衡树/main2.cc
+++ ACWing/253. 普通平衡树/main2.cc
@@ -3,127 +3,123 @@ using namespace std;
 
 const int N = 1e5 + 10, INF = 1e8;
 int n;
-
 struct Node {
-    int l, r;
-    int key, val;
-    int cnt, size;
+  int l, r;
+  int key, val;
+  int cnt, size;
 } tr[N];
 int root, idx;
 int x, y, z;
 
 int get_node(int key) {
-    tr[++idx].key = key;
-    tr[idx].val = rand();
-    tr[idx].size = tr[idx].cnt = 1;
-    return idx;
+  tr[++idx].key = key;
+  tr[idx].val = rand();
+  tr[idx].size = tr[idx].cnt = 1;
+  return idx;
 }
 
 void pushup(int p) {
-    tr[p].size = tr[tr[p].l].size + tr[tr[p].r].size + tr[p].cnt;
+  tr[p].size = tr[tr[p].l].size + tr[tr[p].r].size + tr[p].cnt;
 }
 
 void split(int p, int key, int &x, int &y) {
-    if (!p) x = y = 0;
-    else {
-        if (tr[p].key <= key) {
-            x = p;
-            split(tr[p].r, key, tr[p].r, y);
-        } else {
-            y = p;
-            split(tr[p].l, key, x, tr[p].l);
-        }
-
-        pushup(p);
+  if (!p) x = y = 0;
+  else {
+    if (tr[p].key <= key) {
+      x = p;
+      split(tr[p].r, key, tr[p].r, y);
+    } else {
+      y = p;
+      split(tr[p].l, key, x, tr[p].l);
     }
+
+    pushup(p);
+  }
 }
 
 int merge(int x, int y) {
-    if (!x || !y) return x ^ y;
-    if (tr[x].val > tr[y].val) {
-        tr[x].r = merge(tr[x].r, y);
-        pushup(x);
-        return x;
-    } else {
-        tr[y].l = merge(x, tr[y].l);
-        pushup(y);
-        return y;
-    }
+  if (!x || !y) return x ^ y;
+  if (tr[x].val > tr[y].val) {
+    tr[x].r = merge(tr[x].r, y);
+    pushup(x);
+    return x;
+  } else {
+    tr[y].l = merge(x, tr[y].l);
+    pushup(y);
+    return y;
+  }
 }
 
 void insert(int key) {
-    split(root, key, x, z);
-    split(x, key - 1, x, y);
-    if (!y) y = get_node(key);
-    else {
-        tr[y].cnt++;
-        tr[y].size++;
-    }
+  split(root, key, x, z);
+  split(x, key - 1, x, y);
+  if (!y) y = get_node(key);
+  else {
+    tr[y].cnt++;
+    tr[y].size++;
+  }
 
-    root = merge(merge(x, y), z);
+  root = merge(merge(x, y), z);
 }
 
 void remove(int key) {
-    split(root, key, x, z);
-    split(x, key - 1, x, y);
-    if (y) {
-        tr[y].cnt--;
-        tr[y].size--;
-    }
+  split(root, key, x, z);
+  split(x, key - 1, x, y);
+  if (y) {
+    tr[y].cnt--;
+    tr[y].size--;
+  }
 
-    if (!tr[y].cnt) y = 0;
-    root = merge(merge(x, y), z);
+  if (!tr[y].cnt) y = 0;
+  root = merge(merge(x, y), z);
 }
 
 int get_rank_by_key(int key) {
-    split(root, key - 1, x, y);
-    int rank = tr[x].size + 1;
-    root = merge(x, y);
-    return rank;
+  split(root, key - 1, x, y);
+  int rank = tr[x].size + 1;
+  root = merge(x, y);
+  return rank;
 }
 
 int get_key_by_rank(int rank) {
-    int p = root;
-    while (p) {
-        if (tr[tr[p].l].size >= rank)
-            p = tr[p].l;
-        else if (tr[tr[p].l].size + tr[p].cnt < rank) {
-            rank -= tr[tr[p].l].size + tr[p].cnt;
-            p = tr[p].r;
-        } else return tr[p].key;
-    }
+  int p = root;
+  while (p) {
+    if (tr[tr[p].l].size >= rank) p = tr[p].l;
+    else if (tr[tr[p].l].size + tr[p].cnt < rank) {
+      rank -= tr[tr[p].l].size + tr[p].cnt;
+      p = tr[p].r;
+    } else return tr[p].key;
+  }
 
-    return INF;
+  return INF;
 }
 
 int get_prev(int key) {
-    split(root, key - 1, x, y);
-    int p = x;
-    while (tr[p].r) p = tr[p].r;
-    root = merge(x, y);
-    return tr[p].key;
+  split(root, key - 1, x, y);
+  int p = x;
+  while (tr[p].r) p = tr[p].r;
+  root = merge(x, y);
+  return tr[p].key;
 }
 
 int get_next(int key) {
-    split(root, key, x, y);
-    int p = y;
-    while (tr[p].l) p = tr[p].l;
-    root = merge(x, y);
-    return tr[p].key;
+  split(root, key, x, y);
+  int p = y;
+  while (tr[p].l) p = tr[p].l;
+  root = merge(x, y);
+  return tr[p].key;
 }
 
 int main() {
-    scanf("%d", &n);
-    while (n--) {
-        int opt, a;
-        scanf("%d%d", &opt, &a);
-        if (opt == 1) insert(a);
-        else if (opt == 2) remove(a);
-        else if (opt == 3) printf("%d\n", get_rank_by_key(a));
-        else if (opt == 4) printf("%d\n", get_key_by_rank(a));
-        else if (opt == 5) printf("%d\n", get_prev(a));
-        else if (opt == 6) printf("%d\n", get_next(a));
-    }
-
-    return 0;
+  scanf("%d", &n);
+  while (n--) {
+    int opt, a;
+    scanf("%d%d", &opt, &a);
+    if (opt == 1) insert(a);
+    else if (opt == 2) remove(a);
+    else if (opt == 3) printf("%d\n", get_rank_by_key(a));
+    else if (opt == 4) printf("%d\n", get_key_by_rank(a));
+    else if (opt == 5) printf("%d\n", get_prev(a));
+    else if (opt == 6) printf("%d\n", get_next(a));
+  }
 }
