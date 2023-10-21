dc4951d61f95fc9e64d989bb9fdb9a4c8b6c6304
edwardwang618 <edward.wang618@gmail.com>
2023-10-21 02:39:59 -0700

    ac 2476

--- ACWing/2476. 树套树/main.cc
+++ ACWing/2476. 树套树/main.cc
@@ -4,122 +4,184 @@ using namespace std;
 const int N = 1e5 + 10, INF = 1e9;
 int n, m;
 int a[N];
-namespace Treap {
+
+namespace FHQ {
+
 struct Node {
+#define lc(u) tr[u].l
+#define rc(u) tr[u].r
+#define val(u) tr[u].val
+#define sz(u) tr[u].sz
   int l, r;
-  int key, val;
-  int sz;
-} tr[N * 20];
+  int val, sz;
+  int rnd;
+} tr[N * 21];
 int idx;
-
-void pushup(int u) {
-  tr[u].sz = tr[tr[u].l].sz + tr[tr[u].r].sz + 1;
-}
-
-int get_node(int key) {
-  tr[++idx] = {0, 0, key, rand(), 1};
+int get_node(int v) {
+  tr[++idx] = {0, 0, v, 1, rand()};
   return idx;
 }
 
-void zig(int &p) {
-  int q = tr[p].l;
-  tr[p].l = tr[q].r, tr[q].r = p, p = q;
-  pushup(tr[p].r);
+void pushup(int u) { sz(u) = sz(lc(u)) + sz(rc(u)) + 1; }
+
+void split(int u, int val, int &x, int &y) {
+  if (!u) x = y = 0;
+  else {
+    if (val < val(u)) {
+      y = u;
+      split(lc(u), val, x, lc(u));
+      pushup(y);
+    } else {
+      x = u;
+      split(rc(u), val, rc(u), y);
+      pushup(x);
+    }
+  }
 }
 
-void zag(int &p) {
-  int q = tr[p].r;
-  tr[p].r = tr[q].l, tr[q].l = p, p = q;
-  pushup(tr[p].l);
+int merge(int x, int y) {
+  if (!x || !y) return x | y;
+  else {
+    if (tr[x].rnd > tr[y].rnd) {
+      rc(x) = merge(rc(x), y);
+      pushup(x);
+      return x;
+    } else {
+      lc(y) = merge(x, lc(y));
+      pushup(y);
+      return y;
+    }
+  }
 }
 
-void insert(int &p, int x) {
-  if (!p) {
-    p = get_node(x);
-    return;
+struct FHQ {
+  int root;
+  
+  void insert(int v) {
+    int x, y, z;
+    split(root, v - 1, x, z);
+    y = get_node(v);
+    root = merge(merge(x, y), z);
+  }
+  
+  void modify(int w, int v) {
+    int x, y, z;
+    split(root, w - 1, x, z);
+    split(z, w, y, z);
+    y = merge(lc(y), rc(y));
+    root = merge(merge(x, y), z);
+    insert(v);
   }
 
-  if (x <= tr[p].key) {
-    insert(tr[p].l, x);
-    if (tr[tr[p].l].val > tr[p].val) zig(p);
-  } else if (x > tr[p].key) {
-    insert(tr[p].r, x);
-    if (tr[tr[p].r].val > tr[p].val) zag(p);
+  int get_less(int v) {
+    int x, y;
+    split(root, v - 1, x, y);
+    int res = sz(x);
+    root = merge(x, y);
+    return res;
   }
 
-  pushup(p);
-}
+  int get_prev(int v) {
+    int u = root, res = -INF;
+    while (u) {
+      if (v > val(u)) res = max(res, val(u)), u = rc(u);
+      else u = lc(u);
+    }
+    return res;
+  }
 
-void update(int &p, int k, int x) {
-  if (k <= tr[tr[p].l].sz) update(tr[p].l, k, x);
-  else if (k > tr[tr[p].l].sz + 1) update(tr[p].r, k - tr[tr[p].l].sz - 1, x);
-  else tr[p].key = x;
-}
+  int get_next(int v) {
+    int u = root, res = INF;
+    while (u) {
+      if (v < val(u)) res = min(res, val(u)), u = lc(u);
+      else u = rc(u);
+    }
+    return res;
+  }
+};
 
-int get_prev(int p, int x) {
-  if (!p) return -INF;
-  if (x <= tr[p].key) return get_prev(tr[p].l, x);
-  else return max(tr[p].key, get_prev(tr[p].r, x));
-}
-
-int get_next(int p, int x) {
-  if (!p) return INF;
-  if (x >= tr[p].key) return get_next(tr[p].r, x);
-  else return min(tr[p].key, get_next(tr[p].l, x));
-}
-
-int get_rank(int p, int x) {
-  if (x <= tr[p].key) return count_less(tr[p].l, x);
-  else if (x > tr[p].key) return tr[tr[p].l].sz + 1 + count_less(tr[p].r, x);
-}
-}
+}  // namespace FHQ
 
 namespace SegTree {
+
 struct Node {
   int l, r;
-  int root;
+  FHQ::FHQ fhq;
 } tr[N << 2];
 
 void build(int u, int l, int r) {
-  tr[u] = {l, r, Treap::get_node(a[l])};
+  tr[u] = {l, r};
+  for (int i = l; i <= r; i++) tr[u].fhq.insert(a[i]);
   if (l == r) return;
-  for (int i = l + 1; i <= r; i++) Treap::insert(root, a[i]);
+
   int mid = l + r >> 1;
   build(u << 1, l, mid), build(u << 1 | 1, mid + 1, r);
 }
 
-void modify(int u, int k, int x) {
-  Treap::update(tr[u].root, k, x);
-  if (tr[u].l == tr[u].r) return;
-
-  int mid = tr[u].l + tr[u].r >> 1;
-  if (k <= mid) modify(u << 1, k, x);
-  else modify(u << 1 | 1, k - (tr[u << 1].r - tr[u << 1].l + 1), x);
-}
-
-int count_less(int u, int l, int r, int x) {
-  if (l <= tr[u].l && tr[u].r <= r) return Treap::count_less(tr[u].root, x);
+int get_less(int u, int l, int r, int x) {
+  if (l <= tr[u].l && tr[u].r <= r) return tr[u].fhq.get_less(x);
   int mid = tr[u].l + tr[u].r >> 1;
   int res = 0;
-  if (l <= mid) res = count_less(u << 1, l, r, x);
-  if (r > mid) res += count_less(u << 1 | 1, l, r, x);
+  if (l <= mid) res += get_less(u << 1, l, r, x);
+  if (r > mid) res += get_less(u << 1 | 1, l, r, x);
   return res;
 }
 
-int get_
+int get_key_from_rank(int lq, int rq, int rk) {
+  int l = 0, r = 1e8;
+  while (l < r) {
+    int mid = l + r + 1 >> 1;
+    if (get_less(1, lq, rq, mid) + 1 <= rk) l = mid;
+    else r = mid - 1;
+  }
 
-
+  return l;
 }
+
+void modify(int u, int pos, int x) {
+  if (pos < tr[u].l || pos > tr[u].r) return;
+  tr[u].fhq.modify(a[pos], x);
+  int mid = tr[u].l + tr[u].r >> 1;
+  if (pos <= mid) modify(u << 1, pos, x);
+  else modify(u << 1 | 1, pos, x);
+}
+
+int get_prev(int u, int l, int r, int x) {
+  if (l <= tr[u].l && tr[u].r <= r) return tr[u].fhq.get_prev(x);
+  int mid = tr[u].l + tr[u].r >> 1;
+  int res = -INF;
+  if (l <= mid) res = max(res, get_prev(u << 1, l, r, x));
+  if (r > mid) res = max(res, get_prev(u << 1 | 1, l, r, x));
+  return res;
+}
+
+int get_next(int u, int l, int r, int x) {
+  if (l <= tr[u].l && tr[u].r <= r) return tr[u].fhq.get_next(x);
+  int mid = tr[u].l + tr[u].r >> 1;
+  int res = INF;
+  if (l <= mid) res = min(res, get_next(u << 1, l, r, x));
+  if (r > mid) res = min(res, get_next(u << 1 | 1, l, r, x));
+  return res;
+}
+
+}  // namespace SegTree
 
 int main() {
   scanf("%d%d", &n, &m);
   for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
-  SegTree::build(1, l, r);
+  SegTree::build(1, 1, n);
   while (m--) {
-    int op, l, r, k, x;
+    int op, l, r, pos, x;
     scanf("%d", &op);
-    if (op == 1 || op == 2 || op == 4 || op == 5) scanf("%d%d", &l, &r);
+    if (op == 1 || op == 2 || op == 4 || op == 5) scanf("%d%d%d", &l, &r, &x);
+    else scanf("%d%d", &pos, &x);
 
-
+    if (op == 1) printf("%d\n", SegTree::get_less(1, l, r, x) + 1);
+    else if (op == 2) printf("%d\n", SegTree::get_key_from_rank(l, r, x));
+    else if (op == 3) {
+      SegTree::modify(1, pos, x);
+      a[pos] = x;
+    } else if (op == 4) printf("%d\n", SegTree::get_prev(1, l, r, x));
+    else printf("%d\n", SegTree::get_next(1, l, r, x));
   }
 }
