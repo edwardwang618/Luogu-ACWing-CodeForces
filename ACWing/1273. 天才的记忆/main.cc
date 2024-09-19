cf97a34c613792017f86f11a774e6f42dc740a46
edwardwang618 <edward.wang618@gmail.com>
2024-09-18 17:41:45 -0700

    倍增

--- ACWing/1273. 天才的记忆/main.cc
+++ ACWing/1273. 天才的记忆/main.cc
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <cstring>
 #include <cmath>
+#include <cstring>
+#include <iostream>
 using namespace std;
 
 const int N = 2e5 + 10, M = 19;
@@ -8,20 +8,18 @@ int n, m;
 int f[N][M];
 
 int main() {
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++) scanf("%d", &f[i][0]);
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) scanf("%d", &f[i][0]);
 
-    for (int j = 1; j < M; j++)
-        for (int i = 1; i + (1 << j) - 1 <= n; i++)
-            f[i][j] = max(f[i][j - 1], f[i + (1 << j - 1)][j - 1]);
+  for (int j = 1; 1 << j < n; j++)
+    for (int i = 1; i + (1 << j) - 1 <= n; i++)
+      f[i][j] = max(f[i][j - 1], f[i + (1 << j - 1)][j - 1]);
 
-    scanf("%d", &m);
-    while (m--) {
-        int l, r;
-        scanf("%d%d", &l, &r);
-        int k = log(r - l + 1) / log(2);
-        printf("%d\n", max(f[l][k], f[r - (1 << k) + 1][k]));
-    }
-
-    return 0;
+  scanf("%d", &m);
+  while (m--) {
+    int l, r;
+    scanf("%d%d", &l, &r);
+    int k = log2(r - l + 1);
+    printf("%d\n", max(f[l][k], f[r - (1 << k) + 1][k]));
+  }
 }
