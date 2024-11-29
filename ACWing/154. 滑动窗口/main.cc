c4a67feeea61efc566cd7d51ddb4874ce4e15418
edwardwang618 <edward.wang618@gmail.com>
2024-11-29 00:20:11 -0800

    单调栈、队列

--- ACWing/154. 滑动窗口/main.cc
+++ ACWing/154. 滑动窗口/main.cc
@@ -1,33 +1,31 @@
-#include <iostream>
 #include <deque>
+#include <iostream>
 using namespace std;
 
 const int N = 1000010;
 int a[N];
 
 int main() {
-    int n, k;
-    cin >> n >> k;
+  int n, k;
+  cin >> n >> k;
 
-    for (int i = 0; i < n; i++) cin >> a[i];
+  for (int i = 0; i < n; i++) cin >> a[i];
 
-    deque<int> dq;
+  deque<int> dq;
 
-    for (int i = 0; i < n; i++) {
-        while (!dq.empty() && a[dq.back()] >= a[i]) dq.pop_back();
-        dq.push_back(i);
-        if (dq.front() <= i - k) dq.pop_front();
-        if (i >= k - 1) cout << a[dq.front()] << " ";
-    }
+  for (int i = 0; i < n; i++) {
+    while (!dq.empty() && a[dq.back()] >= a[i]) dq.pop_back();
+    dq.push_back(i);
+    if (dq.front() <= i - k) dq.pop_front();
+    if (i >= k - 1) cout << a[dq.front()] << " ";
+  }
 
-    cout << endl;
-    dq.clear();
-    for (int i = 0; i < n; i++) {
-        while (!dq.empty() && a[dq.back()] <= a[i]) dq.pop_back();
-        dq.push_back(i);
-        if (dq.front() <= i - k) dq.pop_front();
-        if (i >= k - 1) cout << a[dq.front()] << " ";
-    }
-
-    return 0;
+  cout << endl;
+  dq.clear();
+  for (int i = 0; i < n; i++) {
+    while (!dq.empty() && a[dq.back()] <= a[i]) dq.pop_back();
+    dq.push_back(i);
+    if (dq.front() <= i - k) dq.pop_front();
+    if (i >= k - 1) cout << a[dq.front()] << " ";
+  }
 }
--- ACWing/830. 单调栈/main.cc
+++ ACWing/830. 单调栈/main.cc
@@ -6,17 +6,15 @@ const int N = 100010;
 int a[N];
 
 int main() {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> a[i];
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> a[i];
 
-    stack<int> stk;
+  stack<int> stk;
 
-    for (int i = 0; i < n; i++) {
-        while (!stk.empty() && stk.top() >= a[i]) stk.pop();
-        cout << (stk.empty() ? -1 : stk.top()) << ' ';
-        stk.push(a[i]);
-    }
-
-    return 0;
+  for (int i = 0; i < n; i++) {
+    while (!stk.empty() && stk.top() >= a[i]) stk.pop();
+    cout << (stk.empty() ? -1 : stk.top()) << ' ';
+    stk.push(a[i]);
+  }
 }
